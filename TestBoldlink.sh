fe04dc2d0c893ceeb4ba2080a6018b0501acaef7
unknown <Martin KImani@OPS-MARTIN.office.tangazoletu.com>
2021-06-16 18:18:40 +0300

    Test Link

---
+++ TestBoldlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#A bash script to echo Hello world and name Martin Kimani
+echo Hello World!
+
+#Can use a configurable input. Represent by static Name.
+read -p "Enter Your Name: "  Kimani
+echo "Hello $Kimani!"
